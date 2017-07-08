e2d0e1b344030d878a3305b05e6d5613efbd2f2f
makkhawan voraboot <issara.goodnet4733@gmail.com>
2017-07-08 17:27:36 +0700

    improve to es6 format

--- blockchains/broker.js
+++ blockchains/broker.js
@@ -1,10 +1,5 @@
-const fetchUrl = require("fetch").fetchUrl;
-const redis = require("redis");
-const bitcoinrpc = require('bitcoin');
-const rsclient = redis.createClient();
-const kue = require('kue');
-const queue = kue.createQueue();
-const fullnode = require('../config/fullnode.config');
+import bitcoinrpc from 'bitcoin'
+import fullnode from '../config/fullnode.config'
 
 function getBitcoinRPCConnection (connection) {
   const client = new bitcoinrpc.Client({
@@ -20,16 +15,16 @@ function getBitcoinRPCConnection (connection) {
 const bitcoin = {
   connect: (callback) => {
     getBitcoinRPCConnection((connected) => {
-      callback(null, connected);
+      callback(null, connected)
     })
   },
 
   getinfo: () => {
     getBitcoinRPCConnection((connected) => {
       connected.cmd('getinfo', (err, info) => {
-        if (err) return callback(err, null);
-        const infoJSON = JSON.stringify(info);
-        rsclient.set('bitcoinstatus', infoJSON.toString());
+        if (err) return callback(err, null)
+        const infoJSON = JSON.stringify(info)
+        rsclient.set('bitcoinstatus', infoJSON.toString())
       })
     })
   },
@@ -37,9 +32,9 @@ const bitcoin = {
   getrawtransaction: (txid, callback) => {
     getBitcoinRPCConnection((connected) => {
       connected.cmd('getrawtransaction', txid, (err, response) => {
-        if (err) return callback(err, null);
-        const getrawtransaction = JSON.stringify(response);
-        callback(null, getrawtransaction);
+        if (err) return callback(err, null)
+        const getrawtransaction = JSON.stringify(response)
+        callback(null, getrawtransaction)
       })
     })
   },
@@ -47,22 +42,30 @@ const bitcoin = {
   decoderawtransaction: (hex, callback) => {
     getBitcoinRPCConnection((connected) => {
       connected.cmd('decoderawtransaction', hex, (err, response) => {
-        if (err) return callback(err, null);
-        const decoderawtransaction = JSON.stringify(response);
-        callback(null, decoderawtransaction);
+        if (err) return callback(err, null)
+        const decoderawtransaction = JSON.stringify(response)
+        callback(null, decoderawtransaction)
       })
-    }); 
+    })
   },
 
   searchrawtransactions: (address, callback) => {
     getBitcoinRPCConnection((connected) => {
       connected.cmd('searchrawtransactions', address, (err, response) => {
-        if (err) return callback(err, null);
-        const searchrawtransactions = JSON.stringify(response);
-        callback(null, searchrawtransactions);
+        if (err) return callback(err, null)
+        const searchrawtransactions = JSON.stringify(response)
+        callback(null, searchrawtransactions)
       })
-    }); 
+    })
   },
+
+  getblock: (blockhash) => {
+    getBitcoinRPCConnection((connected) => {
+      connected.cmd('getblock', blockhash, (err, response) => {
+        if (err) return callback(err, null)
+      })
+    })
+  }
 
 }
 
